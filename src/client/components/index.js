82be2a38550c2c4ef98bffd4d1aff4bf7a92c31a
Casey Foster <c@sey.me>
2015-09-29 12:05:56 -0500

    And then the cars spoke to each other

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -35,12 +35,9 @@ document.addEventListener('keyup',
 );
 
 export default class extends Component {
-  state = {
-    cars: {}
-  }
-
   constructor(props) {
     super(props);
+    this.cars = {};
     this.peers = {};
     (this.live = new Live())
       .on('peers', ({self, rest}) => {
@@ -48,16 +45,14 @@ export default class extends Component {
         _.each(_.difference(current, rest), id => {
           this.getPeer(id).close();
           delete this.peers[id];
-          this.setState({cars: _.omit(this.state.cars, id)});
+          this.scene.remove(this.getCar(id));
+          delete this.cars[id];
         });
         _.each(_.difference(rest, current), id => {
           if (self > id) this.getPeer(id).call();
         });
       })
       .on('signal', ({id, data}) => this.getPeer(id).signal(data));
-    document.addEventListener('mousemove', ({pageX: x, pageY: y}) =>
-      _.each(this.peers, peer => peer.send('mouse', [x, y]))
-    );
   }
 
   getPeer(id) {
@@ -66,14 +61,20 @@ export default class extends Component {
     if (peer) return peer;
     peer = peers[id] = new Peer({id});
     peer.on('signal', data => this.live.send('signal', {id, data}));
-    peer.on('mouse', _.bind(this.handleMouse, this, id));
+    peer.on('u', _.bind(this.handleUpdate, this, id));
     return peer;
   }
 
-  handleMouse(id, [x, y]) {
-    const {cars} = this.state;
-    cars[id] = {x, y};
-    this.setState({cars});
+  getCar(id) {
+    if (this.cars[id]) return this.cars[id];
+    this.scene.add(this.cars[id] = CarMesh());
+  }
+
+  handleUpdate(id, [px, pz, ry]) {
+    const car = this.getCar(id);
+    car.position.x = px;
+    car.position.z = pz;
+    car.rotation.y = ry;
   }
 
   componentDidMount() {
@@ -89,6 +90,7 @@ export default class extends Component {
     this.ball = new THREE.Vector3(0, 0, 0);
     this.car = CarMesh();
     this.car.gas = 0;
+    this.car.wheel = 0;
     this.car.speed = 0;
     this.scene.add(this.car);
     this.renderMap();
@@ -107,10 +109,29 @@ export default class extends Component {
     else if (pad) {
       const {6: reverse, 7: forward} = pad.buttons;
       this.car.gas = forward.value - reverse.value;
-      this.lastCheck = pad.timestamp;
     } else this.car.gas = 0;
+
+    if (KEYS[37]) this.car.wheel = -1;
+    else if (KEYS[39]) this.car.wheel = 1;
+    else if (pad) {
+      const [wheel] = pad.axes;
+      this.car.wheel = Math.abs(wheel < 0.2) ? 0 : wheel;
+    } else this.car.wheel = 0;
+
     this.car.speed = (this.car.speed + (this.car.gas * 0.1)) * 0.9;
-    this.car.position.z += this.car.speed;
+    const {speed, wheel} = this.car;
+    this.car.rotation.y += speed * wheel * Math.PI * 0.02;
+    this.car.position.x += speed * Math.sin(this.car.rotation.y);
+    this.car.position.z += speed * Math.cos(this.car.rotation.y);
+
+    _.each(this.peers, peer =>
+      peer.send('u', [
+        this.car.position.x,
+        this.car.position.z,
+        this.car.rotation.y
+      ])
+    );
+
     this.updateCamera();
     RENDERER.render(this.scene, CAMERA);
   }
