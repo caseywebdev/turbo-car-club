b05510a0da382e930c812200df084066dc284174
Casey Foster <c@sey.me>
2015-10-23 10:53:53 -0500

    Fix build

--- src/signal/listeners/verify.js
+++ src/signal/listeners/verify.js
@@ -1,32 +1,32 @@
-import * as app from 'signal';
-// import async from 'async';
-import config from 'signal/config';
-import verify from 'shared/utils/verify';
-
-const {invalidKey, keyExpired} = config.errors;
-
-export default (socket, key, cb) => {
-  const data = verify(config.key, key);
-  if (!data || data.type !== 'verify') return cb(invalidKey);
-
-  const {emailAddress, expiresAt, type, userId} = token;
-  if (!emailAddress || !expiresAt || type !== 'verify' || !userId) {
-    return cb(INVALID_TOKEN);
-  }
-
-  if (Date.now() > new Date(expiresAt)) return cb(TOKEN_EXPIRED);
-
-  app.knex('email_addresses')
-    .insert({email_address: emailAddress, user_id: userId});
-  // async.waterfall([
-  //   cb =>
-  //     app.knex.db
-  //       .select('*')
-  //       .from('users')
-  //       .where({id: userId})
-  //       .asCallback(cb),
-  //   ([user], cb) => {
-  //     if (!user) return cb(INVALID_TOKEN);
-  //   }
-  // ], cb);
-};
+// import * as app from 'signal';
+// // import async from 'async';
+// import config from 'signal/config';
+// import verify from 'shared/utils/verify';
+//
+// const {invalidKey, keyExpired} = config.errors;
+//
+// export default (socket, key, cb) => {
+//   const data = verify(config.key, key);
+//   if (!data || data.type !== 'verify') return cb(invalidKey);
+//
+//   const {emailAddress, expiresAt, type, userId} = token;
+//   if (!emailAddress || !expiresAt || type !== 'verify' || !userId) {
+//     return cb(INVALID_TOKEN);
+//   }
+//
+//   if (Date.now() > new Date(expiresAt)) return cb(TOKEN_EXPIRED);
+//
+//   app.knex('email_addresses')
+//     .insert({email_address: emailAddress, user_id: userId});
+//   // async.waterfall([
+//   //   cb =>
+//   //     app.knex.db
+//   //       .select('*')
+//   //       .from('users')
+//   //       .where({id: userId})
+//   //       .asCallback(cb),
+//   //   ([user], cb) => {
+//   //     if (!user) return cb(INVALID_TOKEN);
+//   //   }
+//   // ], cb);
+// };
