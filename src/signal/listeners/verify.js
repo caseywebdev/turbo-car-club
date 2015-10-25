d9777fd68e6bf3b29b064a480d2c6990b7ab7371
Casey Foster <c@sey.me>
2015-10-25 17:05:30 -0500

    keyExpired is gone

--- src/signal/listeners/verify.js
+++ src/signal/listeners/verify.js
@@ -10,7 +10,7 @@ import verify from 'shared/utils/verify';
 
 const {
   key,
-  errors: {invalidKey, keyExpired, unknown},
+  errors: {invalidKey, unknown},
   verifyKeyTtl
 } = config;
 
@@ -21,7 +21,7 @@ export default (socket, signed, cb) => {
     _.partial(findOrCreateUser, {email_address: data.emailAddress}),
     (user, _cb) => {
       if (user.signed_in_at.toISOString() !== data.signedInAt) {
-        return cb(keyExpired);
+        return cb(invalidKey);
       }
       updateSignedInAt(user.id, _cb);
     }
