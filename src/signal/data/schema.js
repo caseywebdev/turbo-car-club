dd35a464944a7cf7f860cc972709af425ff69e37
Casey Foster <c@sey.me>
2016-01-26 20:29:25 -0600

    Update schema

--- src/signal/data/schema.js
+++ src/signal/data/schema.js
@@ -1,5 +1,5 @@
 import {GraphQLSchema} from 'graphql';
 import query from './query';
-import mutation from './mutation';
+// import mutation from './mutation';
 
-export default new GraphQLSchema({query, mutation});
+export default new GraphQLSchema({query, mutation: undefined});
