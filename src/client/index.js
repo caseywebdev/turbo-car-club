9c2425b3212a2da330fb9470dc46b13aee70b996
Casey Foster <c@sey.me>
2016-02-02 11:53:37 -0600

    Add cost calulator

--- src/client/index.js
+++ src/client/index.js
@@ -24,20 +24,6 @@ import './utils/livereload';
 //     .call();
 // };
 
-const queries = [
-  [
-    'hosts',
-    {online: true},
-    {$range: {first: 10}},
-    [
-      'id',
-      ['name', ['first', 'last']],
-      ['owner', ['id', ['name', ['first', 'last']]]]
-    ]
-  ],
-  ['users', 'name', ['first', 'last']]
-];
-
 import _ from 'underscore';
 
 const _flattenPath = (path, prefix = []) => {
@@ -45,24 +31,73 @@ const _flattenPath = (path, prefix = []) => {
 
   const [first, ...rest] = path;
 
-  if (!_.isArray(first)) return _flattenPath(rest, [...prefix, first]);
+  if (!_.isArray(first)) return _flattenPath(rest, prefix.concat(first));
 
-  return _.reduce(first, (paths, path) => [
-    ...paths,
-    ..._flattenPath([
-      ...prefix,
-      ...(_.isArray(path) ? path : [path]),
-      ...rest
-    ])
-  ], []);
+  return _.reduce(
+    first,
+    (paths, path) => paths.concat(_flattenPath(prefix.concat(path, rest))),
+    []
+  );
 };
 
 const flattenPath = path => _flattenPath(path);
 
-const flattenPaths = queries =>
-  _.flatten(_.map(queries, flattenPath), true);
+const flattenPaths = queries => _.flatten(_.map(queries, flattenPath), true);
 
-const routes = {
+const stringToPath = path => _.invoke(path.split('.'), 'split', '|');
+
+const pathToString = path => path.join('.');
+
+const querySegmentToRouteSegment = segment =>
+  _.isNumber(segment) ? [segment, '$key', '$range'] :
+  !_.isObject(segment) ? [segment, '$key'] :
+  _.has(segment, '$range') ? '$range' :
+  '$params';
+
+const queryToRoutes = query => {
+  const {length} = query;
+  const flattened = flattenPath(_.map(query, querySegmentToRouteSegment));
+  return _.unique(_.reduce(_.range(length), (routes, n) => routes.concat(
+    _.map(flattened, route => pathToString(route.slice(0, length - n)))
+  ), [])).concat('$fallback');
+};
+
+const addRoute = (router, fn, path) =>
+  _.reduce(
+    flattenPath(stringToPath(path)),
+    (router, path) => ({...router, [pathToString(path)]: fn}),
+    router
+  );
+
+const ROUTE_NOT_FOUND_ERROR = new Error('No matching route found');
+const EXPENSIVE_QUERY_ERROR = new Error('Query is too expensive');
+const DEFAULT_FALLBACK = () => { throw ROUTE_NOT_FOUND_ERROR; };
+
+const createRouter = routes =>
+  _.reduce(_.extend({$fallback: DEFAULT_FALLBACK}, routes), addRoute, {});
+
+const run = ({maxCost, router, queries, context}) => {
+  limitQueriesCost(queries, maxCost);
+  const runs = _.reduce(flattenPaths(queries), ({runs, queries}, query) => {
+    const route = _.find(queryToRoutes(query), _.partial(_.has, router));
+    const routePath = stringToPath(route);
+    const arity = route === '$fallback' ? 0 : routePath.length;
+    if (arity && query.length > arity) queries.push(query);
+    const fn = router[route];
+    let run = _.find(runs, {fn});
+    if (!run) runs.push(run = {fn, args: {context, queries: []}});
+    run.args.queries.push(query);
+    _.times(arity, n => {
+      run.args[n] = (run.args[n] || []).concat(
+        _.any(run.args[n], _.partial(_.isEqual, query[n])) ? [] : query[n]
+      );
+    });
+    return {runs, queries};
+  }, {runs: [], queries: []});
+  return Promise.resolve(runs);
+};
+
+const router = createRouter({
   'hosts.$params.$range':
     ({1: params}) => ({path: ['hosts', params, 0], value: 'Foo!'}),
   'hostsById.$key.id|name|owner':
@@ -76,75 +111,52 @@ const routes = {
   'usersByid.$key.id|name|emailAddress':
     ({context, 1: ids, 2: keys}) =>
       context && ids && keys
+});
+
+const queries = [
+  [
+    'hosts',
+    {online: true},
+    {$range: {from: 0, size: 10}},
+    {$range: {from: 0, size: 10}},
+    [
+      'id',
+      ['name', ['first', 'last']],
+      ['owner', ['id', 'name']]
+    ]
+  ],
+  ['user', ['id', 'name']]
+];
+
+const limitQueryCost = (query, max, precost = 0) => {
+  if (!query.length) return precost;
+
+  const [first, ...rest] = query;
+
+  const cost =
+    _.isArray(first) ?
+    _.reduce(first, (cost, path) =>
+      cost + limitQueryCost([].concat(path, rest), max, precost)
+    , 0) :
+    limitQueryCost(rest, max, precost + 1);
+
+  if (cost > max) throw EXPENSIVE_QUERY_ERROR;
+
+  return cost;
 };
 
-const stringToPath = path => _.invoke(path.split('.'), 'split', '|');
-const pathToString = path => path.join('.');
+const limitQueriesCost = (queries, max) =>
+  _.reduce(queries, (cost, query) => {
+    cost += limitQueryCost(query, max);
 
-const querySegmentToRouteSegment = segment =>
-  !_.isObject(segment) ? [segment, '$key'] :
-  _.has(segment, '$range') ? '$range' :
-  '$params';
+    if (cost > max) throw EXPENSIVE_QUERY_ERROR;
 
-const queryToRoutes = query => {
-  const {length} = query;
-  const flattened = flattenPath(_.map(query, querySegmentToRouteSegment));
-  return _.unique(_.reduce(_.range(length), (routes, n) => [
-    ...routes,
-    ..._.map(flattened, route => pathToString(route.slice(0, length - n)))
-  ], [])).concat('$fallback');
-};
+    return cost;
+  }, 0);
 
-const addRoute = (router, fn, path) =>
-  _.reduce(
-    flattenPath(stringToPath(path)),
-    (router, path) => ({...router, [pathToString(path)]: fn}),
-    router
-  );
-
-const ROUTE_NOT_FOUND_ERROR = new Error('No matching route found');
-const DEFAULT_FALLBACK = () => { throw ROUTE_NOT_FOUND_ERROR; };
-
-const createRouter = routes =>
-  _.reduce(_.extend({$fallback: DEFAULT_FALLBACK}, routes, addRoute, {}));
-
-const router = createRouter(routes);
-
-const run = (router, queries, context) => {
-  const runMap = _.reduce(flattenPaths(queries), (runMap, query) => {
-    const route = _.find(queryToRoutes(query), _.partial(_.has, router));
-    const arity = route === '$fallback' ? Infinity : stringToPath(route).length;
-    const unfulfilled = query.length > arity;
-    const fn = router[route];
-    const runFn = map.get(fn);
-    if (!runFn) {
-      map.set(fn, {
-        context,
-        arity,
-        queries: [],
-        unfulfilled: [],
-        ..._.times(arity, n => [])
-      });
-    }
-    if (!map.has(fn))
-  }, new Map());
-  return Promise.resolve(_.reduce(queries, (map, {query, fn}) => {
-    if (!map.has(fn)) map.set(fn, {context, queries: []});
-    map.get(fn).queries.push(query);
-    const remainder =
-    _.each(query, (segment, i) => {
-      if (!map.get(fn)[i]) map.get(fn)[i] = [];
-      const existing = _.find(map.get(fn)[i], _.partial(_.isEqual, segment));
-      if (!existing) map.get(fn)[i].push(segment);
-    });
-    return map;
-  }, new Map()));
-  // Promise.all(_.map(flattenPaths(queries), query => {
-  //   const route = _.find(queryToRoutes(query), _.partial(_.has, routes));
-  //   return Promise.resolve({context, query})
-  //     .then(routes[route] || fallback)
-  //     .catch($error => ({path: query, value: {$error}}));
-  // }));
-};
-
-run(router, queries).then(console.log.bind(console));
+run({
+  maxCost: 100,
+  router,
+  queries,
+  context: {userId: 1}
+}).then(console.log.bind(console));
