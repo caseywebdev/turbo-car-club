4450befdc3fd9d458c83d6b81faa0c0a51ef46ab
Casey Foster <c@sey.me>
2016-02-02 12:20:22 -0600

    Sort object keys

--- src/shared/utils/stringify.js
+++ src/shared/utils/stringify.js
@@ -1,12 +1,8 @@
 import _ from 'underscore';
 
-const sortObj = obj => {
-  if (!_.isObject(obj)) return obj;
-  if (_.isArray(obj)) return _.map(obj, sortObj);
-  return _.reduce(_.sortBy(_.keys(obj)), (newObj, key) => {
-    newObj[key] = sortObj(obj[key]);
-    return newObj;
-  }, {});
-};
+const sortObj = obj =>
+  _.isArray(obj) ? _.map(obj, sortObj) :
+  _.isObject(obj) ? _.sortBy(_.pairs(_.mapObject(obj, sortObj)), 0) :
+  obj;
 
 export default obj => JSON.stringify(sortObj(obj));
