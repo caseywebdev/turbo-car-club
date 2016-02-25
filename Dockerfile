ef0e7581ec3a12e512759e51f3f1fb31a73ec12e
Casey Foster <c@sey.me>
2016-02-25 17:02:47 -0600

    Update deps

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,5 @@
 FROM node:5
-
-# Workaround until https://github.com/npm/npm/issues/9863 is resolved.
-RUN curl -L https://npmjs.org/install.sh | npm_install=2 sh
-
 WORKDIR /code
-
-COPY Makefile /code/Makefile
-COPY package.json /code/package.json
-RUN make install
-
 COPY . /code
 RUN MINIFY=1 make
-
 CMD ["bin/host"]
--- src/client/components/host.js
+++ src/client/components/host.js
@@ -1,17 +1,28 @@
+import _ from 'underscore';
 import cx from '../utils/cx';
 import getUserDisplayName from '../../shared/utils/get-user-display-name';
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 const {host: cxl} = cx;
 
-const render = ({host: {owner, name}}) =>
-  <div className={cxl.root}>
-    <div className={cxl.name}>{name}</div>
-    <div className={cxl.owner}>{getUserDisplayName(owner)}</div>
-  </div>;
+const render = _.extend(
+  ({host: {owner, name}}) =>
+    <div className={cxl.root}>
+      <div className={cxl.name}>{name}</div>
+      <div className={cxl.owner}>{getUserDisplayName(owner)}</div>
+    </div>,
+  {
+    propTypes: {
+      host: PropTypes.shape({
+        owner: PropTypes.object.isRequired,
+        name: PropTypes.string.isRequired
+      }).isRequired
+    },
 
-render.fragments = () => ({
-  host: ['name', ['owner', ['id', 'name']]]
-});
+    fragments: () => ({
+      host: ['name', ['owner', ['id', 'name']]]
+    })
+  }
+);
 
 export default render;
