23e85a276f199fa3366b1a27f7975a1962950667
Casey Foster <c@sey.me>
2016-03-26 00:59:52 -0500

    Simplify set name

--- src/client/components/set-name.js
+++ src/client/components/set-name.js
@@ -1,33 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import store from '../utils/store';
 
-export default class extends Component {
-  state = {
-    error: null,
-    isLoading: false
-  };
+const handleKeyDown = ({key, target: {value: name}}) => {
+  if (key === 'Enter') store.run({query: ['user!', {name}]});
+};
 
-  handleKeyDown({key, target: {value: name}}) {
-    if (key !== 'Enter') return;
-
-    this.setState({
-      error: null,
-      isLoading: true
-    });
-
-    store
-      .run({query: ['user!', {name}]})
-      .then(() => {
-        console.log('name set');
-      })
-      .catch(er => {
-        console.error(er);
-      });
-  }
-
-  render() {
-    return (
-      <input onKeyDown={::this.handleKeyDown} />
-    );
-  }
-}
+export default () =>
+  <input
+    defaultValue={store.get(['user', 'name'])}
+    onKeyDown={handleKeyDown}
+  />;
