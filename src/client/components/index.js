5433f71105a24ed7070f0ab7f40a637ba4816119
Casey Foster <c@sey.me>
2016-05-10 10:44:35 -0500

    Add logo

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,9 +1,13 @@
+import cx from '../utils/cx';
 import React from 'react';
 import Hosts from './hosts';
 import User from './user';
 
+const {index: cxl} = cx;
+
 export default () =>
   <div>
+    <img className={cxl.logo} src='/gfx/logo.svg' />
     <User />
     <Hosts />
   </div>;
